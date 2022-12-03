706bb2c85575453ceac1d3f9264efb1052fcbcc9
bphuongggg <80316539+bphuongggg@users.noreply.github.com>
2022-12-03 14:02:08 +0700

    create banner ads and social media

--- plant-seeds-home-project/src/pages/Home/Home.js
+++ plant-seeds-home-project/src/pages/Home/Home.js
@@ -3,11 +3,20 @@ import axios from 'axios';
 
 import products1 from '../../assets/items.json';
 import BASE_API_URL from '../../api/api';
+import {
+    faFacebook,
+    faInstagram,
+    faYoutube,
+    faTwitter,
+    faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
 
 import SliderProduct from '../../components/SliderProduct/SliderProduct';
 import styles from './Home.module.scss';
 import Categories from '../../components/Categories/Categories';
 import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import bannerAds from '../../assets/images/banner-ads.png';
 
 const cx = classNames.bind(styles);
 
@@ -34,6 +43,45 @@ function Home() {
             <div className={cx('wrapper')}>
                 <Categories />
                 <SliderProduct items={products} title={'News products'}></SliderProduct>
+            </div>
+            {/* floating icon */}
+            <div className={cx('icons')}>
+                <ul>
+                    <a href="">
+                        <li className={cx('facebook')}>
+                            <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+                        </li>
+                    </a>
+                    <a href="">
+                        <li className={cx('twitter')}>
+                            <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+                        </li>
+                    </a>
+                    <a href="">
+                        <li className={cx('youtube')}>
+                            <FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon>
+                        </li>
+                    </a>
+                    <a href="">
+                        <li className={cx('linkedin')}>
+                            <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                        </li>
+                    </a>
+                    <a href="">
+                        <li className={cx('instagram')}>
+                            <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+                        </li>
+                    </a>
+                </ul>
+            </div>
+            <div className={cx('banner-ads')}>
+                <img
+                    src="https://www.ruffalonl.com/wp-content/uploads/2020/11/Mizzou_Vertical-Banner.jpg"
+                    alt="ads"
+                    // width={120}
+                    // height={110}
+                />
+                {/* quang cao */}
             </div>
         </div>
     );
